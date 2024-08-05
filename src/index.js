4168dd176ec79a3e47ffcd88cc7cbe141eafef53
Pablo González González <pablo.glez.dev@gmail.com>
2024-08-05 10:51:11 +0100

    WIP pinpad developing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-function test(a, b) {
-  return a + b;
+// const divTest = document.createElement('div');
+// const textTest = document.createTextNode('prueba');
+
+// divTest.append(textTest);
+
+// const pinpadModal = document.getElementById('pinpad');
+// pinpadModal.append(divTest);
+
+const { localStorage } = window;
+
+const isPinCreated = localStorage.getItem('pin-created-at');
+console.log(isPinCreated);
+
+// TODO change condition to is not created
+if (!isPinCreated) {
+  alert('Pin is not created');
+  localStorage.setItem('pin-created-at', new Date().toISOString());
+
+  const welcomeContainer = document.getElementById('welcome-container');
+  welcomeContainer.hidden = false;
+
+  const welcomeDialog = document.getElementById('welcome-dialog');
+  welcomeDialog.showModal();
 }
 
-console.log(test(1, 2));
+// eslint-disable-next-line no-unused-vars
+function closeModal() {
+  const welcomeDialog = document.getElementById('welcome-dialog');
+  welcomeDialog.close();
+
+  const welcomeContainer = document.getElementById('welcome-container');
+  welcomeContainer.hidden = true;
+}
